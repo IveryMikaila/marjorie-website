c3bd63436a39fa92c78578073ac3e672d6ed2f37
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-02-28 17:40:46 -0500

    Added Homepage Content

--- website/src/Components/Home.js
+++ website/src/Components/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 import '../Styles/Home.css'
 import heroWeb from '../assets/brokenVesselsWeb.svg';
 import heroMobile from '../assets/brokenVesselsMobile.svg';
+import bookReviews from '../assets/bookReviews.png';
 import Slider from './Slider';
+import About from './About';
 
 const Home = () => {
   return (
@@ -11,7 +13,32 @@ const Home = () => {
 <img className='hero-web' src={heroWeb} alt='Introduction to Restoring Broken Vessels Website Web Version'/>
 <img className='hero-mobile' src={heroMobile} alt='Introduction to Restoring Broken Vessels Website Mobile Version' />
 {/* Amazon Store */}
+<div className='amazon-section'>
+
 <Slider />
+
+<a target='_blank' className='amazon-link' href='https://www.amazon.com/Quest-Find-Whom-Soul-Loves/dp/B08R4FB8NC'>
+<img className='reviews-img' src={bookReviews} alt='Amazon book reviews and description'/>
+</a>
+</div>
+<p className='amazon-text'>Check out my books on <a className='store-link' target='_blank' href='https://www.amazon.com/stores/Marjorie-McAllister/author/B08RP8N7JG?ref=ap_rdr&isDramIntegrated=true&shoppingPortalEnabled=true'> Amazon</a>✨</p>
+<About />
+
+<div className='event-section'>
+  <p>
+  Join us for a day of inspiration, empowerment, and transformation at The Epiphany - “An Awakening” Women's Conference!
+<br/>
+<br/>
+Date: Saturday, May 18
+<br/>
+Time: 10:00 AM - 2:00 PM
+<br/>
+Location:
+240 Naugatuck Avenue,
+Milford, CT 06460
+</p>
+
+</div>
     </>
   )
 }
