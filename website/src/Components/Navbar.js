93af5af834f60d616bccd1f27f60a1674cc731f6
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-02-27 19:08:29 -0500

    Optimized Navbar for mobile screens

--- website/src/Components/Navbar.js
+++ website/src/Components/Navbar.js
@@ -1,17 +1,22 @@
 import React,{useState} from 'react'
 import { Squash as Hamburger } from 'hamburger-react'
+import '../Styles/Navbar.css'
 
 const Navbar = () => {
 const [open, setOpen] = useState(false);
 
   return (
-    <>
+    <div className='navigation'>
     <Hamburger toggled={open} toggle={setOpen} />
+
 <div className={open ? 'nav-active' : 'nav' }>
-Test
+  <div className='nav-links'>
+  <a className='nav-link' href='/'>Home</a>
+<a className='nav-link' href='/contact'>Contact</a>
+  </div>
 </div>
     
-    </>
+    </div>
   )
 }
 
