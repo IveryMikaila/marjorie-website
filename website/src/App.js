14cd81e5abe320ce3588c51a7bab83d56a744b70
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-02-29 15:01:45 -0500

    Created Thank You Page for Contact Form

--- website/src/App.js
+++ website/src/App.js
@@ -4,6 +4,7 @@ import {Routes,Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Contact from './Components/Contact'
+import Submit from './Components/Submit';
 import Footer from './Components/Footer';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 <Routes>
 <Route element={<Home/>} path='/' />
 <Route element={<Contact/>} path='/contact' />
+<Route element={<Submit/>} path='/thankyou'/>
 </Routes>
 <Footer />
     </div>
