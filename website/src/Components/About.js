b441d6c2a9e1d252dd59a0caac787e3968686baf
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-03-11 15:40:17 -0400

    Removed Homepage design

--- website/src/Components/About.js
+++ website/src/Components/About.js
@@ -4,15 +4,7 @@ const About = () => {
   return (
     <>
     <div className='about'>
-        <p>
-        At Restoring Broken Vessels, we believe in the transformative power of biblical mentoring. We understand that life's challenges can often feel overwhelming, but we are here to offer guidance, support and hope through the wisdom of Scripture. Our mentor, Marjorie McAllister is an elder of her church, a graduate of the University of Bridgeport, and an author of 2 books "On a Quest to Find Him Whom My Soul Loves" and "On a Quest to Find Him Whom My Soul Loves: The Journey Begins". 
-        </p>
-        <p>
-        Her mentoring approach is rooted in the timeless truths found in the Bible. She believes that God's Word provides the ultimate source of wisdom, healing, and restoration for individuals, couples, and families facing various struggles and difficulties. Whether you are grappling with issues related to relationships, mental health, sexual assault, church hurt, or any other concern, Marjorie is here to walk alongside you on your journey toward wholeness and healing.
-        </p>
-        <p>
-        If you are ready to take the first step toward healing and restoration, we encourage you to reach out to us today. You don't have to face life's challenges alone. We're here to help you find hope, healing, and wholeness through the transformative power of the word of God. Contact us to schedule your appointment or learn more about our services. We look forward to walking alongside you on your journey toward healing and freedom.
-        </p>
+
     </div>
     </>
   )
--- website/src/Components/Contact.js
+++ website/src/Components/Contact.js
@@ -7,9 +7,10 @@ const Contact = () => {
     <div className='contact-container'>
 <h1>Let your healing journey begin.</h1>
 <p>If you have any questions regarding mentoring, our women's conference or to RSVP please indicate your interest in the contact form below. </p>
+
+{/* Contact Form */}
 <div className='contact-form'>
-<form action="https://formsubmit.co/marjoriemcallsiter@gmail.com" method="POST">
-<input type="hidden" name="_next" value="https://restoringbrokenvessels.netlify.app/thankyou"/>
+<form  action="https://formsubmit.co/mynkie123@gmail.com" method="POST">
 <input type="hidden" name="_subject" value="New Contact Form Submission" />
 <label for="name">Full Name:</label>
      <input id='name' type="text" name="Full Name" required />
@@ -19,9 +20,9 @@ const Contact = () => {
      <input id='number' type='tel' name="Phone Number" required />
      <label for="message">Message:</label>
      <textarea className='contact-textbox' id="message" name="Message" rows="20" cols="50"></textarea>
+     <input type="hidden" name="_next" value="https://restoringbrokenvessels.netlify.app/thankyou"/>
      <button type="submit">Send</button>
 </form>
-
 </div>
     </div>
   
